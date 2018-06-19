66ddc7d288b9a17b35b0fa08cfb921813b08cabb
Nastia Nedosiekova <Anastasiia_Nedosekova@govitall.com>
2018-06-19 15:57:18 +0300

    Add tests for coupon code

--- src/pages.py
+++ src/pages.py
@@ -1,10 +1,14 @@
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class MainPage(object):
+    # Better to use blocks when there are so much elements& If I was writing this from scratch I would use blocks
+    # because there are to much functions for one instance
     def __init__(self, driver):
         self.driver = driver
         self.builder = ActionChains(self.driver)
@@ -12,17 +16,27 @@ class MainPage(object):
         self.discount_label_locator = "//*[@class = 'percent']"
         self.accept_locator = "//button[contains(text(), 'Accept')]"
         self.total_price_locator = "//div[contains(@class, 'total-price-all')]"
+        self.coupon_link_locator = "//a[contains(@class, 'js-coupon_link')]"
+        self.coupon_locator = "//*[@id= 'coupon']"
+        self.coupon_result_locator = "//*[contains(@class, 'coupon__result')]"
 
     def open(self):
         self.driver.get("https://mackeeper.com/buy-now-bensolo")
+        self.accept_terms()
         return self
 
     def add_discount_to_url(self, discount):
         url = self.driver.current_url + '&printOfDiscount=' + discount
         self.driver.get(url)
 
+    def add_coupon_to_url(self, coupon):
+        url = self.driver.current_url + '&couponcode=' + coupon
+        self.driver.get(url)
+        self.wait_for_coupon_result()
+
         # Better to writer wrapper function - such as clickElement(and wait for visibility of element there)
         # and not to write WebdriverWait all the time
+
     def is_discount_label_exist(self):
         WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.XPATH,
                                                                                self.discount_label_locator)))
@@ -30,20 +44,45 @@ class MainPage(object):
         return percent.is_displayed()
 
     def accept_terms(self):
-        accept = self.driver.find_element_by_xpath(self.accept_locator)
-        self.builder.click(accept).perform()
-        WebDriverWait(self.driver, 20).until(EC.invisibility_of_element_located((By.XPATH, self.accept_locator)))
-        WebDriverWait(self.driver, 20).until(lambda x: self.page_has_loaded())
+        if self.is_accept_terms_exist():
+            accept = self.driver.find_element_by_xpath(self.accept_locator)
+            self.builder.click(accept).perform()
+            WebDriverWait(self.driver, 20).until(EC.invisibility_of_element_located((By.XPATH, self.accept_locator)))
+            WebDriverWait(self.driver, 20).until(lambda x: self.page_has_loaded())
+
+    def is_accept_terms_exist(self):
+        try:
+            self.driver.find_element_by_xpath(self.accept_locator)
+        except NoSuchElementException:
+            return False
+        return True
 
     def go_to_checkout(self):
-        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.XPATH, self.instant_activation_locator)))
+        WebDriverWait(self.driver, 30).until(
+            EC.visibility_of_element_located((By.XPATH, self.instant_activation_locator)))
         instant_activation = self.driver.find_element_by_xpath(self.instant_activation_locator)
         self.builder.move_to_element(instant_activation).perform()
         self.builder.click(instant_activation).perform()
         return CheckoutPage(self.driver)
 
     def get_total_price(self):
-        return self.driver.find_element_by_xpath(self.total_price_locator).get_attribute('value')
+        WebDriverWait(self.driver, 30).until(
+            EC.visibility_of_element_located((By.XPATH, self.total_price_locator)))
+        return self.driver.find_element_by_xpath(self.total_price_locator).get_attribute('innerText')
+
+    def set_coupon_code(self, discount_code):
+        coupon_link = self.driver.find_element_by_xpath(self.coupon_link_locator)
+        coupon_link.click()
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.XPATH, self.coupon_locator)))
+        coupon = self.driver.find_element_by_xpath(self.coupon_locator)
+        self.builder.move_to_element(coupon)
+        coupon.clear()
+        coupon.send_keys(discount_code)
+        coupon.send_keys(Keys.ENTER)
+        self.wait_for_coupon_result()
+
+    def wait_for_coupon_result(self):
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.XPATH, self.coupon_result_locator)))
 
     # Should be in Class Page (in order to use in all pages)
     def page_has_loaded(self):
@@ -57,7 +96,7 @@ class CheckoutPage(object):
         self.total_element_locator = 'cbp_ID0ENA_ID0EAAABADBBAAOA'
 
     def get_total(self):
-        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.ID,
+        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.ID,
                                                                                self.total_element_locator)))
         total = self.driver.find_element_by_id(self.total_element_locator)
-        return total.get_attribute('value')
+        return total.get_attribute('innerText')
--- tests/test_coupon.py
+++ tests/test_coupon.py
@@ -3,13 +3,11 @@ import pytest
 from src.pages import MainPage
 
 
-@pytest.mark.nondestructive
 def test_affid_in_url(selenium):
     MainPage(selenium).open()
     assert "affid" in selenium.current_url
 
 
-@pytest.mark.nondestructive
 def test_guid_in_url(selenium):
     MainPage(selenium).open()
     assert "guid" in selenium.current_url
@@ -21,11 +19,31 @@ def test_set_print_of_discount_in_url(selenium):
     assert main_page.is_discount_label_exist() is True
 
 
+def test_set_coupon_on_code_in_url(selenium):
+    main_page = MainPage(selenium).open()
+    old_price = main_page.get_total_price()
+    main_page.add_coupon_to_url('rec0v')
+    assert old_price != main_page.get_total_price()
+
+
+def test_price_is_changing_when_entering_coupon(selenium):
+    main_page = MainPage(selenium).open()
+    old_price = main_page.get_total_price()
+    main_page.set_coupon_code('rec0v')
+    assert main_page.get_total_price() != old_price
+
+def test_coupon_is_added_in_checkout(selenium):
+    main_page = MainPage(selenium).open()
+    main_page.set_coupon_code('rec0v')
+    main_page_price = main_page.get_total_price()
+    checkout_page = main_page.go_to_checkout()
+    assert checkout_page.get_total() in main_page_price
+
+
 # Sometimes there are stale elementException. I think it is because of reloaded page after submitting terms
 # I think better to accept terms - setting cookies in JS
 def test_currency_is_the_same(selenium):
     main_page = MainPage(selenium).open()
     main_page_price = main_page.get_total_price()
-    main_page.accept_terms()
     checkout_page = main_page.go_to_checkout()
     assert checkout_page.get_total() in main_page_price
